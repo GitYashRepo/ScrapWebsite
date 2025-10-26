51fee1d3cfb44e877e3a7607790e027350708317
yashrepogit <yas@cloudify.biz>
2025-10-26 13:38:30 +0530

    fixing upload error

--- frontend/src/app/api/blob-upload/route.js
+++ frontend/src/app/api/blob-upload/route.js
@@ -1,11 +1,11 @@
-import { genUploadUrl } from "@vercel/blob";
+import { createUploadUrl } from "@vercel/blob";
 
 export async function GET() {
   try {
     // Create a one-time upload URL
-    const { url } = await genUploadUrl({
+    const { url } = await createUploadUrl({
       access: "public",
-      token: process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN, // explicitly required in v2
+      token: process.env.BLOB_READ_WRITE_TOKEN, // explicitly required in v2
     });
 
     return new Response(JSON.stringify({ url }), { status: 200 });
